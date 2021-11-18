f5a8b8b06b3f5a90a840d6214d5f72c46f1e7cbc
Fabio <fabioaoad@gmail.com>
2021-11-18 12:33:42 -0300

    Login de usuario desde Angular en local  con el backend tambien en  local - AuthApp Angular - Seccion 25, apartado 9

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -17,14 +18,22 @@ export class LoginComponent{
 
 
   constructor( private fb: FormBuilder,
-               private  router: Router ) { }
+               private  router: Router,
+               private authService: AuthService) { }
 
 
   login(){
     console.log(this.miFormulario.value);
-   // console.log(this.miFormulario.valid);
+    //extraigo el emial y password del formulario
+   const { email, password } = this.miFormulario.value;
+   console.log(this.miFormulario.valid);
+    this.authService.login( email, password )
+      .subscribe( resp => {
+        console.log(resp);
+      });
+   // this.router.navigateByUrl('/dashboard');
 
-    this.router.navigateByUrl('/dashboard');
+
   }
 
 }
